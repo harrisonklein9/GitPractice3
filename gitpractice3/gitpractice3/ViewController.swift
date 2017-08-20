78684a886d5a64ee1864bfaef01272a957ff6589
Harrison Klein <bassismything9@mierda.local>
2017-08-19 23:29:35 -0400

    Added new project

---
+++ gitpractice3/gitpractice3/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  gitpractice3
+//
+//  Created by Harrison Klein on 8/19/17.
+//  Copyright © 2017 Harrison Klein. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
