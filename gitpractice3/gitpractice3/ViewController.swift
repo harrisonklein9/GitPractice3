2cd403d2913e1c455c54b3181c35bf5fca69e07f
Harrison Klein <bassismything9@mierda.local>
2017-08-19 23:36:21 -0400

    new stuff

--- gitpractice3/gitpractice3/ViewController.swift
+++ gitpractice3/gitpractice3/ViewController.swift
@@ -14,6 +14,9 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    func harry () {
+        print("harrison klein")
+    }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
